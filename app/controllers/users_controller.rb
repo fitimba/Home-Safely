b91f87f1dbb2a3cbcea66ca68c1fc609b61f19c5
Lynn Kremer <lynnkremer@hotmail.com>
2014-04-13 15:43:43 +0100

    added views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,25 @@
 class UsersController < ApplicationController
 
+	def new
+		@message = 'Sign up here'
+	end
+
 	def create
-		@user = User.create(user_params)
+		@user = User.new
+		@user.email = params[:email]
+		@user.name = params[:name]
+		@user.password = params[:password]
+		@user.contact_1_name = params[:contact_1_name]
+		@user.contact_2_name = params[:contact_2_name]
+		@user.contact_3_name = params[:contact_3_name]
+		@user.longitude = params[:longitude]
+		@user.latitude = params[:latitude]
+		@user.contact_1_number = params[:contact_1_number]
+		@user.contact_2_number = params[:contact_2_number]
+		@user.contact_3_number = params[:contact_3_number]
+		@user.save
 		session[:user_id] = @user.id
 		redirect_to new_contact_path
 	end
 
-	def user_params
-		params.permit(:name, :contact_1_name, :contact_1_number, 
-			:contact_2_name, :contact_2_number,
-			:contact_3_name, :contact_3_number)
-	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :email,
+  	:name, 
+  	:password, 
+  	:contact_1_name,  
+  	:contact_1_number,
+  	:contact_2_name,  
+  	:contact_2_number,
+  	:contact_3_name,  
+  	:contact_3_number,
+  	:latitude,
+  	:longitude
+
 end
